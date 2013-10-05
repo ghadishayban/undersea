60ae3afc9688320165466a5aaa2ba768411c1d48
Ghadi Shayban <gshayban@gmail.com>
2013-10-05 15:15:45 -0400

    Initial repo

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject undersea "0.1.0-SNAPSHOT"
+  :description "Decentralized REPL data sharing"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure.core.async "0.1.242.0-44b1e3-alpha" :exclusions [org.clojure/clojurescript]]
+                 [clj-http "0.7.7"]
+                 [ring/ring-core "1.2.0"]
+                 [ring/ring-jetty-adapter "1.2.0"]])
---
+++ test/undersea/core_test.clj
@@ -0,0 +1,7 @@
+(ns undersea.core-test
+  (:require [clojure.test :refer :all]
+            [undersea.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
